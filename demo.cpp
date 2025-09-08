17de614ee2843e570a374f143923ae6566efac03
Monu Chaudhary <monuchaudharypoonia@gmail.com>
2025-09-08 17:22:33 +0530

    third change

--- demo.cpp
+++ demo.cpp
@@ -6,15 +6,6 @@ using namespace std;
 int main()
 {
     vector<int> numbers = {5, 3, 8, 1, 4};
-    sort(numbers.begin(), numbers.end());
-
-    cout << "Sorted numbers: ";
-    for (const auto &num : numbers)
-    {
-        cout << num << " ";
-        
-    }
-    cout << endl;
 
     return 0;
 }
